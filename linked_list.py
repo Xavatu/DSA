6235adca67a40a2b5591e6f4d2d32832b6af4a84
xavatu <xavatu@gmail.com>
2024-02-02 19:59:07 +0300

    add delete method for LinkedList

--- linked_list.py
+++ linked_list.py
@@ -36,7 +36,24 @@ class LinkedList:
         return []
 
     def delete(self, val, all=False):
-        pass  # здесь будет ваш код
+        node = self.head
+        previous_node = None
+        while node is not None:
+            if node.value == val:
+                if self.head == node:  # if first (head)
+                    self.head = node.next
+                if self.tail == node:  # if last (tail)
+                    self.tail = previous_node
+                tmp_node = node
+                node = tmp_node.next
+                if previous_node is not None:  # if not first (previous exists)
+                    previous_node.next = node
+                del tmp_node
+                if not all:  # continue if we should delete all matches
+                    return
+            else:
+                previous_node = node
+                node = node.next
 
     def clean(self):
         pass  # здесь будет ваш код
