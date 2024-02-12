4d4961b7f7c2421d6af2e5151ffd84a313519627
xavatu <xavatu@gmail.com>
2024-02-12 21:24:30 +0300

    doubly linked list done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -4,6 +4,12 @@ class Node:
         self.prev = None
         self.next = None
 
+    def __repr__(self):
+        return (f"Node(hash={self.__hash__()}, "
+                f"value={self.value}, "
+                f"prev={self.prev.__hash__() if self.prev else None}, "
+                f"next={self.next.__hash__() if self.next else None})")
+
 
 class LinkedList2:
     def __init__(self):
@@ -20,23 +26,71 @@ class LinkedList2:
             item.prev = self.tail
         self.tail = item
 
-    def find(self, val):
-        return None  # здесь будет ваш код
+    def find(self, val) -> Node | None:
+        for node in self:
+            if node.value == val:
+                return node
+        return None
 
     def find_all(self, val):
-        return []  # здесь будет ваш код
+        return [node for node in self if node.value == val]
 
     def delete(self, val, all=False):
-        pass  # здесь будет ваш код
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                if self.head == node:  # if first (head)
+                    self.head = node.next
+                    node.next.prev = None
+                if self.tail == node:  # if last (tail)
+                    self.tail = node.prev
+                if node.prev is not None:
+                    node.prev.next = node.next
+                    if node.next is not None:
+                        node.next.prev = node.prev
+                tmp_node = node
+                node = tmp_node.next
+                del tmp_node
+                if not all:  # continue if we should delete all matches
+                    return
+            else:
+                node = node.next
 
     def clean(self):
-        pass  # здесь будет ваш код
+        for node in self:
+            del node
 
     def len(self):
-        return 0  # здесь будет ваш код
+        return len([node for node in self])
 
     def insert(self, afterNode, newNode):
-        pass  # здесь будет ваш код
+        if afterNode is None:
+            if self.tail is None:
+                self.head = newNode
+                self.tail = newNode
+            else:
+                self.tail.next = newNode
+                newNode.prev = self.tail
+                self.tail = newNode
+            return
+        newNode.next = afterNode.next
+        if afterNode.next is not None:
+            afterNode.next.prev = newNode
+        newNode.prev = afterNode
+        afterNode.next = newNode
+        if self.tail == afterNode:
+            self.tail = newNode
 
     def add_in_head(self, newNode):
-        pass  # здесь будет ваш код
+        newNode.next = self.head
+        self.head.prev = newNode
+        self.head = newNode
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def _get_nodes(self) -> list[Node]:
+        return [node for node in self]
---
+++ doubly_linked_list/test.py
@@ -0,0 +1,214 @@
+import pytest
+from doubly_linked_list import LinkedList2, Node
+
+
+@pytest.fixture()
+def get_empty_list():
+    return LinkedList2()
+
+
+@pytest.fixture()
+def get_list_with_one_element():
+    list_ = LinkedList2()
+    node = Node(v=1)
+    list_.head = node
+    list_.tail = node
+    return list_
+
+
+@pytest.fixture()
+def get_list_with_several_elements():
+    list_ = LinkedList2()
+    node1 = Node(v=1)
+    node2 = Node(v=2)
+    node3 = Node(v=3)
+    node4 = Node(v=2)
+    node5 = Node(v=4)
+    node6 = Node(v=1)
+    node7 = Node(v=7)
+    node1.next = node2
+    node2.prev = node1
+    node2.next = node3
+    node3.prev = node2
+    node3.next = node4
+    node4.prev = node3
+    node4.next = node5
+    node5.prev = node4
+    node5.next = node6
+    node6.prev = node5
+    node6.next = node7
+    node7.prev = node6
+    list_.head = node1
+    list_.tail = node7
+    return list_
+
+
+@pytest.fixture()
+def get_node():
+    return Node(v=1)
+
+
+def test_add_in_tail_empty_list(get_empty_list, get_node):
+    list_ = get_empty_list
+    node = get_node
+    list_.add_in_tail(node)
+    assert list_.head == node
+    assert list_.tail == node
+
+
+def test_add_in_tail_list_with_several_elements(get_list_with_several_elements,
+                                                get_node):
+    list_ = get_list_with_several_elements
+    node = get_node
+    prev_tail = list_.tail
+    list_.add_in_tail(node)
+    assert list_.tail == node
+    assert node.prev == prev_tail
+
+
+def test_find_empty_list(get_empty_list):
+    list_ = get_empty_list
+    assert list_.find(1) is None
+
+
+def test_find_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    nodes = list_._get_nodes()
+    assert list_.find(10) is None
+    assert list_.find(2) == nodes[1]
+    assert list_.find(1) == nodes[0]
+
+
+def test_find_all_empty_list(get_empty_list):
+    list_ = get_empty_list
+    assert list_.find_all(1) == []
+
+
+def test_find_all_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    nodes = list_._get_nodes()
+    assert list_.find_all(10) == []
+    assert list_.find_all(2) == [nodes[1], nodes[3]]
+    assert list_.find_all(1) == [nodes[0], nodes[5]]
+
+
+def test_delete_one_list_with_one_element(get_list_with_one_element):
+    list_ = get_list_with_one_element
+    list_.delete(1)
+    assert list_.head is None
+    assert list_.tail is None
+
+
+def test_delete_one_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    nodes = list_._get_nodes()
+    tail = list_.tail
+    list_.delete(1)
+    assert list_.head == nodes[1]
+    assert list_.tail == tail
+    assert [node for node in list_] == [node for node in nodes if
+                                        node != nodes[0]]
+    list_.delete(1)
+    assert [node for node in list_] == [node for node in nodes if
+                                        node not in (
+                                            nodes[0], nodes[5])]
+    assert nodes[4].next == nodes[6]
+    list_.delete(7)
+    assert [node for node in list_] == [node for node in nodes if
+                                        node not in (
+                                            nodes[0], nodes[5], nodes[6])]
+    assert list_.tail == nodes[4]
+    assert nodes[4].next is None
+
+
+def test_delete_all_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    nodes = list_._get_nodes()
+    # 1, 2, 3, 2, 4, 1, 7
+    tail = list_.tail
+    list_.delete(1, all=True)
+    assert list_.head == nodes[1]
+    assert list_.tail == tail
+    assert [node for node in list_] == [node for node in nodes if
+                                        node not in (
+                                            nodes[0], nodes[5])]
+    assert nodes[4].next == nodes[6]
+    nodes = list_._get_nodes()
+    # 2, 3, 2, 4, 7
+    list_.delete(2, all=True)
+    # 3, 4, 7
+    assert list_.head == nodes[1]
+    assert nodes[1].next == nodes[3]
+    assert [node for node in list_] == [node for node in nodes if
+                                        node not in (nodes[0], nodes[2])]
+
+
+def test_clean_list_with_one_element(get_list_with_one_element):
+    list_ = get_list_with_one_element
+    list_.clean()
+    assert list_.head is None
+    assert list_.tail is None
+
+
+def test_clean_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    list_.clean()
+    assert list_.head is None
+    assert list_.tail is None
+
+
+def test_len_empty_list(get_empty_list):
+    list_ = get_empty_list
+    assert list_.len() == 0
+
+
+def test_len_list_with_one_element(get_list_with_one_element):
+    list_ = get_list_with_one_element
+    assert list_.len() == 1
+
+
+def test_len_list_with_several_elements(get_list_with_several_elements):
+    list_ = get_list_with_several_elements
+    assert list_.len() == 7
+
+
+def test_insert_empty_list(get_empty_list, get_node):
+    list_ = get_empty_list
+    node = get_node
+    list_.insert(None, node)
+    assert list_.head == node
+    assert list_.tail == node
+
+
+def test_insert_after_none_list_with_one_element(get_list_with_one_element,
+                                                 get_node):
+    list_ = get_list_with_one_element
+    nodes = list_._get_nodes()
+    node = get_node
+    list_.insert(None, node)
+    assert list_.tail == node
+    assert node.prev == nodes[0]
+
+
+def test_insert_after_node_list_with_one_element(get_list_with_one_element,
+                                                 get_node):
+    list_ = get_list_with_one_element
+    nodes = list_._get_nodes()
+    node = get_node
+    list_.insert(nodes[0], node)
+    assert list_.head == nodes[0]
+    assert list_.tail == node
+    assert list_.tail.prev == list_.head
+    assert nodes[0].next == node
+
+
+def test_insert_list_with_several_elements(get_list_with_several_elements,
+                                           get_node):
+    list_ = get_list_with_several_elements
+    nodes = list_._get_nodes()
+    node = get_node
+    list_.insert(nodes[2], node)
+    assert nodes[2].next == node
+    assert node.prev == nodes[2]
+    assert node.next == nodes[3]
+    assert nodes[3].prev == node
