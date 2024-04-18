ee2ec1ec8e85906ae70b2f10141f2b44b57c06ae
xavatu <xavatu@gmail.com>
2024-04-18 11:51:13 +0300

    bracket_generator O(n * 2^n) ;(

--- recursion/bracket_generator.py
+++ recursion/bracket_generator.py
@@ -1,52 +1,43 @@
-def _generate_bracket_sequences(
-    i: int, _sequences: list[str] = []
-) -> list[str]:
-    print(i)
+import datetime
+
+
+def _insert_brackets(bracket_sequence: str, i: int) -> str:
+    bracket_sequence = bracket_sequence[:i] + "()" + bracket_sequence[i:]
+    return bracket_sequence
+
+
+def _generate_bracket_sequences(i: int, _sequences: list[str]) -> list[str]:
+    if i < 0:
+        return _sequences
     if i == 0:
-        _sequences.append(f"(0)1")
+        _sequences.append("()")
         return _sequences
     _generate_bracket_sequences(i - 1, _sequences)
     for j in range(len(_sequences)):
-        print(f"{_sequences[j]=} replace={i-1}")
-        _sequences[j] = _sequences[j].replace(str(i - 1), f"({i}){i}")
-        print("y", _sequences[j])
+        sequence = _sequences.pop(0)
+        new1 = _insert_brackets(sequence, i)
+        new2 = _insert_brackets(sequence, i + 1)
+        _sequences.append(new1)
+        _sequences.append(new2)
     return _sequences
 
 
-print(_generate_bracket_sequences(3))
+def generate_bracket_sequences(n: int):
+    return _generate_bracket_sequences(n - 1, [])
 
-# (a)b
-#
-# ()(b)c
-# ((a))c
-#
-# ()((b))d
-# ()()(c)d
-# (((a)))d
-# (())(c)d
-#
-# ()(((b)))e
-# ()(())(d)e
-# ()()((c))e
-# ()()()(d)e
-# ((((a))))e
-# ((()))(d)e
-# (())((c))e
-# (())()(d)e
-#
-# ()
-# 01
-#
-# ()()
-# 0101
-# (())
-# 0011
-#
-# ()(())
-# 010011
-# ()()()
-# 010101
-# ((()))
-# 000111
-# (())()
-# 001101
+
+if __name__ == "__main__":
+    print("N\ttime")
+    for i in range(21):
+        time1 = datetime.datetime.now()
+        result = generate_bracket_sequences(i)
+        time2 = datetime.datetime.now()
+        print(f"{i}\t{str((time2 - time1).total_seconds()).replace(".", ",")}")
+        # result.sort()
+        # print(result)
+        # result2 = [el.replace("(", "0").replace(")", "1") for el in result]
+        # print(result2)
+        # result3 = [int(el, 2) for el in result2]
+        # print(result3)
+        # for i in range(1, len(result3)):
+        #     print(f"{result3[i]} - {result3[i-1]} = {result3[i]-result3[i-1]}")
