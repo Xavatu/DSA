760659e8ec2bfe29b06a5a974328531dafaa5c7b
xavatu <xavatu@gmail.com>
2024-04-18 13:33:21 +0300

    remove mutable at arguments

--- recursion/files.py
+++ recursion/files.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 
 
-def _get_all_files(path: Path, _paths: list[Path] = []):
+def get_all_files(path: Path, _paths=None):
+    if _paths is None:
+        _paths = []
     if path.is_dir():
         for _path in path.iterdir():
-            _get_all_files(_path, _paths)
+            get_all_files(_path, _paths)
     else:
         _paths.insert(0, path)
     return _paths
-
-
-def get_all_files(path: Path) -> list[Path]:
-    return _get_all_files(path)
