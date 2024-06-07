cd93d7f69304454a55a1b0acf7c4315d9608dcbb
xavatu <xavatu@gmail.com>
2024-06-07 20:21:41 +0300

    add BalancedBST

--- tree/balanced_binary_search_tree.py
+++ tree/balanced_binary_search_tree.py
@@ -1,39 +1,5 @@
 from collections import deque
-
-
-def _gen_bbst_tree_recursive(array, queue, max_depth):
-    if not queue:
-        return
-    node_index, left, right, depth = queue.popleft()
-    if node_index is not None:
-        yield array[node_index]
-        if left <= node_index - 1:
-            queue.append(
-                (
-                    left + (node_index - left) // 2,
-                    left,
-                    node_index - 1,
-                    depth + 1,
-                )
-            )
-            max_depth = depth + 1
-        elif depth < max_depth:
-            queue.append((None,) * 4)
-        if node_index + 1 <= right:
-            queue.append(
-                (
-                    node_index + (right - node_index) // 2 + 1,
-                    node_index + 1,
-                    right,
-                    depth + 1,
-                )
-            )
-            max_depth = depth + 1
-        elif depth < max_depth:
-            queue.append((None,) * 4)
-    else:
-        yield None
-    yield from _gen_bbst_tree_recursive(array, queue, max_depth)
+from typing import TypeVar
 
 
 def _gen_bbst_tree(array, queue):
@@ -77,3 +43,68 @@ def GenerateBBSTArray(a: list[int]) -> list[int]:
     queue = deque()
     queue.append((len(sorted_a) // 2, 0, len(sorted_a) - 1, 0))
     return [el for el in _gen_bbst_tree(sorted_a, queue) if el is not None]
+
+
+TypeBSTNode = TypeVar("TypeBSTNode", bound="BSTNode")
+TypeBalancedBST = TypeVar("TypeBalancedBST", bound="BalancedBST")
+
+
+class BSTNode:
+    def __init__(self, key: int, parent: TypeBSTNode | None):
+        self.NodeKey = key
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+        self.Level = 0
+
+
+class BalancedBST:
+    def __init__(self):
+        self.Root: BSTNode | None = None
+
+    def _generate_tree_from_bbst_array(
+        self, array: list, i: int, parent: BSTNode | None
+    ):
+        node = BSTNode(key=array[i], parent=parent)
+        node.Level = parent.Level + 1 if parent else 0
+        left_index = 2 * i + 1
+        if left_index < len(array):
+            node.LeftChild = self._generate_tree_from_bbst_array(
+                array, left_index, node
+            )
+        right_index = 2 * i + 2
+        if right_index < len(array):
+            node.RightChild = self._generate_tree_from_bbst_array(
+                array, right_index, node
+            )
+        return node
+
+    def GenerateTree(self, a: list[int]):
+        queue = deque()
+        queue.append((len(a) // 2, 0, len(a) - 1, 0))
+        tree = _gen_bbst_tree(sorted(a), queue)
+        self.Root = self._generate_tree_from_bbst_array(tree, 0, None)
+
+    def _tree_level(self, root_node: BSTNode):
+        if root_node is None:
+            return 0
+        return max(
+            root_node.Level,
+            self._tree_level(root_node.LeftChild),
+            self._tree_level(root_node.RightChild),
+        )
+
+    def IsBalanced(self, root_node: BSTNode):
+        if root_node is None:
+            return True
+        return (
+            self.IsBalanced(root_node.LeftChild)
+            and self.IsBalanced(root_node.RightChild)
+            and (
+                abs(
+                    self._tree_level(root_node.LeftChild)
+                    - self._tree_level(root_node.RightChild)
+                )
+                <= 1
+            )
+        )
--- tree/test_balanced_binary_search_tree.py
+++ tree/test_balanced_binary_search_tree.py
@@ -1,7 +1,12 @@
 import pytest
 from collections import deque
 
-from tree.balanced_binary_search_tree import GenerateBBSTArray, _gen_bbst_tree
+from tree.balanced_binary_search_tree import (
+    GenerateBBSTArray,
+    _gen_bbst_tree,
+    BalancedBST,
+)
+from tree.binary_search_tree import BSTIterator
 
 
 @pytest.mark.parametrize(
@@ -21,3 +26,10 @@ def test_generate_bbst(expected):
     queue.append((len(sorted_a) // 2, 0, len(sorted_a) - 1, 0))
     assert _gen_bbst_tree(sorted_a, queue) == expected_bbst
     assert GenerateBBSTArray(sorted(expected)) == expected
+    tree = BalancedBST()
+    tree.GenerateTree(expected)
+    assert [el.NodeKey for el in BSTIterator(tree.Root, "bfs")] == expected_bbst
+    assert tree.IsBalanced(tree.Root)
+    if tree.Root.LeftChild:
+        tree.Root.LeftChild = None
+        assert not tree.IsBalanced(tree.Root)
