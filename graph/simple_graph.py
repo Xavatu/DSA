223295dc154e267fdf81fcc21addaa2a689c5753
xavatu <xavatu@gmail.com>
2024-06-24 00:39:34 +0300

    add SimpleGraph

---
+++ graph/simple_graph.py
@@ -0,0 +1,58 @@
+class Vertex:
+    def __init__(self, val: int):
+        self.Value = val
+
+    def __eq__(self, other):
+        if isinstance(other, Vertex):
+            return self.Value == other.Value
+        return False
+
+
+class SimpleGraph:
+    def __init__(self, size: int):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex: list[Vertex | None] = [None] * size
+        self._size = 0
+
+    def AddVertex(self, v: int):
+        if self._size == self.max_vertex:
+            raise ValueError("Graph is full")
+        empty_slot_index = self.vertex.index(None)
+        self.vertex[empty_slot_index] = Vertex(v)
+        self._size += 1
+
+    def RemoveVertex(self, v: int):
+        if not 0 <= v < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        self.vertex[v] = None
+        for i in range(self.max_vertex):
+            self.m_adjacency[i][v] = 0
+        self.m_adjacency[v] = [0 for _ in range(self.max_vertex)]
+
+    def IsEdge(self, v1: int, v2: int) -> bool:
+        if not 0 <= v1 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        if not 0 <= v2 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        return bool(self.m_adjacency[v1][v2] and self.m_adjacency[v2][v1])
+
+    def AddEdge(self, v1: int, v2: int):
+        if not 0 <= v1 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        if not 0 <= v2 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        if self.vertex[v1] is None:
+            raise IndexError(f"No vertex with index {v1}")
+        if self.vertex[v2] is None:
+            raise IndexError(f"No vertex with index {v2}")
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1: int, v2: int):
+        if not 0 <= v1 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        if not 0 <= v2 < self.max_vertex:
+            raise IndexError("Index out of bounds")
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
---
+++ graph/test_simple_graph.py
@@ -0,0 +1,111 @@
+import pytest
+
+from graph.simple_graph import Vertex, SimpleGraph
+
+
+@pytest.fixture()
+def get_empty_graph():
+    return SimpleGraph(size=10)
+
+
+#     6 — — — *
+#     |       |
+#     0 — 1 — 3
+#     |   |   |
+#     |   8   |
+#     |   |   |
+#     |   9 — 2
+#     |       |
+#     * — — — *
+
+
+@pytest.fixture()
+def get_graph():
+    graph = SimpleGraph(size=10)
+    graph.vertex = [
+        Vertex(0),
+        Vertex(1),
+        Vertex(2),
+        Vertex(3),
+        None,
+        None,
+        Vertex(6),
+        None,
+        Vertex(8),
+        Vertex(9),
+    ]
+    graph.m_adjacency = [
+        [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    ]
+    return graph
+
+
+def test_add_vertex(get_empty_graph, get_graph):
+    empty_graph = get_empty_graph
+    empty_graph.AddVertex(0)
+    assert empty_graph.vertex[0] is not None
+    assert empty_graph.m_adjacency[0] == [0 for _ in range(10)]
+    assert [empty_graph.m_adjacency[0][i] for i in range(10)] == [
+        0 for _ in range(10)
+    ]
+    graph = get_graph
+    graph.AddVertex(4)
+    assert graph.vertex[4] is not None
+    assert graph.m_adjacency[4] == [0 for _ in range(10)]
+    assert [graph.m_adjacency[4][i] for i in range(10)] == [
+        0 for _ in range(10)
+    ]
+    graph.AddVertex(5)
+    graph.AddVertex(7)
+    try:
+        graph.AddVertex(8)
+    except ValueError:
+        assert True
+
+
+def test_remove_vertex(get_graph):
+    graph = get_graph
+    graph.RemoveVertex(0)
+    assert graph.vertex[0] is None
+    assert graph.IsEdge(0, 1) is False
+    assert graph.IsEdge(1, 0) is False
+    assert graph.IsEdge(0, 2) is False
+    assert graph.IsEdge(2, 0) is False
+    assert graph.IsEdge(0, 6) is False
+    assert graph.IsEdge(6, 0) is False
+
+
+def test_is_edge(get_graph):
+    graph = get_graph
+    assert graph.IsEdge(0, 1) is True
+    assert graph.IsEdge(1, 2) is False
+    assert graph.IsEdge(2, 9) is True
+    assert graph.IsEdge(9, 2) is True
+
+
+def test_add_edge(get_graph):
+    graph = get_graph
+    assert graph.IsEdge(0, 8) is False
+    graph.AddEdge(0, 8)
+    assert graph.IsEdge(0, 8) is True
+
+
+def test_remove_edge(get_graph):
+    graph = get_graph
+    assert graph.IsEdge(0, 1) is True
+    graph.RemoveEdge(0, 1)
+    assert graph.IsEdge(0, 1) is False
+    assert graph.IsEdge(1, 0) is False
+    assert graph.IsEdge(2, 9) is True
+    graph.RemoveEdge(9, 2)
+    assert graph.IsEdge(9, 2) is False
+    assert graph.IsEdge(2, 9) is False
