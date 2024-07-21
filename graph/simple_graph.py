535c5ae435afb6d5ecc839adb8cf22aa2012b73b
xavatu <xavatu@gmail.com>
2024-07-21 18:06:07 +0300

    add BFS for SimpleGraph

--- graph/simple_graph.py
+++ graph/simple_graph.py
@@ -59,7 +59,7 @@ class SimpleGraph:
         self.m_adjacency[v1][v2] = 0
         self.m_adjacency[v2][v1] = 0
 
-    def _depth_first_search(self, vfrom, vto):
+    def _depth_first_search(self, vfrom: int, vto: int) -> deque[int]:
         cur = vfrom
         dq = deque((vfrom,))
         while dq:
@@ -68,14 +68,14 @@ class SimpleGraph:
                 self.vertex[vto].Hit = True
                 dq.append(vto)
                 return dq
-            found = False
+            unvisited = False
             for i in range(self.max_vertex):
                 if self.m_adjacency[cur][i] and not self.vertex[i].Hit:
                     cur = i
                     dq.append(i)
-                    found = True
+                    unvisited = True
                     break
-            if not found:
+            if not unvisited:
                 dq.pop()
                 cur = dq[-1] if dq else None
         return dq
@@ -88,3 +88,40 @@ class SimpleGraph:
                 continue
             el.Hit = False
         return [self.vertex[i] for i in self._depth_first_search(VFrom, VTo)]
+
+    def _breadth_first_search(self, vfrom: int, vto: int) -> deque[int]:
+        cur = vfrom
+        dq = deque((vfrom,))
+        prev = [-1] * self.max_vertex
+        while True:
+            self.vertex[cur].Hit = True
+            if cur == vto:
+                break
+            unvisited = False
+            for i in range(self.max_vertex):
+                if self.m_adjacency[cur][i] and not self.vertex[i].Hit:
+                    dq.append(i)
+                    prev[i] = cur
+                    self.vertex[i].Hit = True
+                    unvisited = True
+                    break
+            if not dq:
+                return dq
+            if not unvisited:
+                cur = dq.popleft()
+        dq.clear()
+        cur = vto
+        while prev[cur] != -1:
+            dq.appendleft(cur)
+            cur = prev[cur]
+        dq.appendleft(vfrom)
+        return dq
+
+    def BreadthFirstSearch(self, VFrom: int, VTo: int) -> list[Vertex]:
+        if self.vertex[VFrom] is None or self.vertex[VTo] is None:
+            return []
+        for el in self.vertex:
+            if not el:
+                continue
+            el.Hit = False
+        return [self.vertex[i] for i in self._breadth_first_search(VFrom, VTo)]
--- graph/test_simple_graph.py
+++ graph/test_simple_graph.py
@@ -118,3 +118,12 @@ def test_dfs(get_graph):
     assert [el.Value for el in graph.DepthFirstSearch(0, 9)] == [0, 1, 3, 2, 9]
     graph.AddVertex(4)
     assert [el.Value for el in graph.DepthFirstSearch(0, 4)] == []
+
+
+def test_bfs(get_graph):
+    graph = get_graph
+    assert [el.Value for el in graph.BreadthFirstSearch(0, 1)] == [0, 1]
+    assert [el.Value for el in graph.BreadthFirstSearch(0, 8)] == [0, 1, 8]
+    assert [el.Value for el in graph.BreadthFirstSearch(0, 9)] == [0, 2, 9]
+    graph.AddVertex(4)
+    assert [el.Value for el in graph.BreadthFirstSearch(0, 4)] == []
